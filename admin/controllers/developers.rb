7758653ab6233e3337b8dcd9fb1e234a618cf21e
Taco van Dijk <taco@Orbo>
2012-06-08 16:35:50 +0200

    find bug

--- admin/controllers/developers.rb
+++ admin/controllers/developers.rb
@@ -5,8 +5,9 @@ Admin.controllers :developers do
     @account = current_account
     
     @contexts = SharedDataContext.all #TODO: you can only edit/delete the ones that you manage..
-
-    @applications = SharedDataApplication.by_account(:account => @account.id, :descending => true)
+    #this is the correct way to do a search on account!
+    #FIXME: should be a search the id only, the account should not be saved nested fashion here
+    @applications = SharedDataApplication.by_account(:key => @account, :descending => true)
     render "developer/index"
   end
 end

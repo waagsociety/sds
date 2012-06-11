620650319adb05eb8916f5a5be36f5a0b69342c8
Taco van Dijk <taco@Orbo>
2012-06-11 11:29:29 +0200

    flow + user list

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -1,6 +1,15 @@
 Admin.controllers :base do
 
   get :index, :map => "/" do
+    
+
+    if(current_account.role == "dataowner")
+    	redirect "admin/users"	
+    end
+
+    if(current_account.role == "developer")
+        redirect "admin/developers"
+    end
     render "base/index"
   end
 end
--- admin/controllers/users.rb
+++ admin/controllers/users.rb
@@ -1,8 +1,11 @@
 Admin.controllers :users do
 
   get :index, :map => "/users" do
-    #render "base/index"
-	"Hello Data Store Owner"
+	@account = current_account
+	store = PersonalStore.find_by_account_id(@account.id)
+	@contexts = store.personal_contexts
+    	#@applications = SharedDataApplication.by_account(:key => @account, :descending => true)
+	render 'user/index'
   end
 
 end

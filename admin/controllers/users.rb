bf1af4c9665d0fd1e289804136004a5c2694eaca
Taco van Dijk <taco@Orbo>
2012-06-12 11:19:22 +0200

    revoke/grant

--- admin/controllers/users.rb
+++ admin/controllers/users.rb
@@ -4,8 +4,27 @@ Admin.controllers :users do
 	@account = current_account
 	store = PersonalStore.find_by_account_id(@account.id)
 	@contexts = store.personal_contexts
-    	#@applications = SharedDataApplication.by_account(:key => @account, :descending => true)
 	render 'user/index'
   end
 
+  #revoke access of a client 
+  put :revoke, :map => "/users/revoke" do
+	  @account = current_account
+	  pca = PersonalContextAuthorization.find_by_access_token(params[:id])
+	  if(pca.state == 2)
+		 pca.state = 3 #revoke this application
+		 pca.save 
+	  end 
+	  redirect 'admin/users'
+  end
+
+  put :grant, :map => "/users/grant" do
+	  @account = current_account
+	  pca = PersonalContextAuthorization.find_by_access_token(params[:id])
+	  if(pca.state != 2)
+		 pca.state = 2 #grant access to this application
+		 pca.save 
+	  end 
+	  redirect 'admin/users'
+  end
 end
